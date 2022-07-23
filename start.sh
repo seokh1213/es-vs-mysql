83d1ad7334e8166d5e51b4b0f81fbd60c85a5106
seokh1213 <seokh1213@gmail.com>
2022-07-23 20:38:38 +0900

    feature: setting docker

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
-docker-compose -f docker/docker-compose.yml down
-docker-compose -f docker/docker-compose.yml up -d
-docker cp es01:/usr/share/elasticsearch/config/certs/es01/es01.crt ./src/main/resources/http_ca.crt
+docker-compose -f docker/es-docker-compose.yml down
+docker volume rm -f docker_certs docker_esdata01 docker_esdata02 docker_esdata03 docker_kibanadata
+docker-compose -f docker/es-docker-compose.yml up -d
+docker-compose -f docker/mysql-docker-compose.yml down
+docker volume rm -f docker_mysql8
+docker-compose -f docker/mysql-docker-compose.yml up -d
+docker cp es01:/usr/share/elasticsearch/config/certs/es01/es01.crt ./http_ca.crt
+keytool -delete -noprompt -cacerts -alias escert -storepass changeit
+keytool -importcert -noprompt -alias escert -cacerts -file http_ca.crt -storepass changeit # default password changeit
