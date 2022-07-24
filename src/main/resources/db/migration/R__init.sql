de02bc31fd8f0afec36caa32cf15e92340d24633
seokh1213 <seokh1213@gmail.com>
2022-07-24 13:45:02 +0900

    feature: flyway 설정 중

---
+++ src/main/resources/db/migration/R__init.sql
@@ -0,0 +1,14 @@
+drop table post_entity if exists;
+create table post_entity
+(
+    uid        bigint auto_increment
+        primary key,
+    author     varchar(255)  not null,
+    content    varchar(1000) not null,
+    created_dt datetime(6)   not null,
+    title      varchar(255)  not null
+);
+
+create fulltext index titleAndContent
+    on post_entity (title, content);
+
